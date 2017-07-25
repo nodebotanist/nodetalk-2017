33840548b41889723b13d110bf4c2596d099e5c8
Kas Perch <the@nodebotani.st>
2017-07-24 22:26:45 -0700

    Queue works

--- code/bot/index.js
+++ code/bot/index.js
@@ -1,15 +1,24 @@
 const Consumer = require('sqs-consumer')
+const AWS = require('aws-sdk')
 
 const Dotstar = require('./dotstar')
 const secrets = require('./secrets')
+
+AWS.config.update({
+  region: 'us-west-1',
+  correctClockSkew: true,
+  accessKeyId: secrets.aws_access_key_id,
+  secretAccessKey: secrets.aws_secret_access_key
+})
 
 const app = Consumer.create({
   queueUrl: 'https://sqs.us-west-1.amazonaws.com/007381580642/LED-Bar',
   handleMessage: (message, done) => {
     // ...
-    done()
-  }
-})
+    done();
+  },
+  sqs: new AWS.SQS()
+});
 
 let strip = new Dotstar(20, 'A')
 
