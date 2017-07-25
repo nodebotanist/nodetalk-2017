2ffa93af890a0aaa5c77e9fea264cae3271066d9
Kas Perch <the@nodebotani.st>
2017-07-24 23:15:28 -0700

    Lambda adding github events to SQS

--- code/lambda/handler.js
+++ code/lambda/handler.js
@@ -1,16 +1,22 @@
-'use strict';
+var QUEUE_URL = 'https://sqs.us-west-1.amazonaws.com/007381580642/LED-Bar'
+var AWS = require('aws-sdk')
+var sqs = new AWS.SQS({region : 'us-west-1'})
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+module.exports.hello = function(event, context, cb) {
+  var params = {
+    MessageBody: JSON.stringify(event),
+    QueueUrl: QUEUE_URL
+  }
+  sqs.sendMessage(params, function(err, data){
+    if(err) {
+      cb('error: Fail Send Message' + err)
+    }else{
+      cb(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          id: data.MessageId
+        })
+      });  // SUCCESS
+    }
+  })
+}
