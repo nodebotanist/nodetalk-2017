33beeb29670ff8f139220c505d1dfd3c486c9caf
Kas Perch <the@nodebotani.st>
2017-07-24 21:59:08 -0700

    Remowed need for secrets, added queue test

--- code/bot/queue-test.js
+++ code/bot/queue-test.js
@@ -1,7 +1,5 @@
 const Consumer = require('sqs-consumer')
-
-const Dotstar = require('./dotstar')
-const secrets = require('./secrets')
+const AWS = require('aws-sdk')
 
 const app = Consumer.create({
   queueUrl: 'https://sqs.us-west-1.amazonaws.com/007381580642/LED-Bar',
@@ -11,20 +9,8 @@ const app = Consumer.create({
   }
 })
 
-let strip = new Dotstar(20, 'A')
-
-strip.init(4000000)
-strip.clear()
-
-strip.setAll({
-  brightness: 0x99,
-  red: 0x66,
-  green: 0x33,
-  blue: 0x99
-})
-
 app.on('error', (err) => {
-  console.log(err.message);
+  console.log(err.message)
 })
 
 app.start()
